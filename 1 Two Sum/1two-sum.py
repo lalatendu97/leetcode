6c5318c3a0c00add9ca0694562bd74977df9c22b
Lalatendu Bal <lalatendu96@gmail.com>
2026-01-09 18:57:46 +0000

    Code challenge submission for question: 1 Two Sum

--- 1 Two Sum/1two-sum.py
+++ 1 Two Sum/1two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
         for i, val in enumerate(nums):
             val2 = target - val
-            if val2 in nums:
-                j = nums.index(val2)
-                if i == j:
-                    continue
-                return [i, j]
-        
+            if val2 in seen:
+                return [seen[val2], i]
+            seen[val] = i
+                
