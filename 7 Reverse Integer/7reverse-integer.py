0c61f9aac58465f69af721b24e0d98a79658e134
Lalatendu Bal <lalatendu96@gmail.com>
2026-01-10 11:34:49 +0000

    Code challenge submission for question: 7 Reverse Integer

--- 7 Reverse Integer/7reverse-integer.py
+++ 7 Reverse Integer/7reverse-integer.py
@@ -2,7 +2,6 @@ class Solution:
     def reverse(self, x: int) -> int:
         sign = -1 if x < 0 else 1
         int_M = 2147483648
-        int_m = -2147483648
         rev = 0
         x *= sign
         c = len(str(x))
@@ -12,12 +11,9 @@ class Solution:
             x = x //10
             rev = rev*10 + digit
         
-        rev *= sign
-        
         if c == 10:
             if rev + 1 > int_M:
                 return 0
-            if rev < int_m:
-                return 0
-        return rev
+            
+        return sign * rev
             
