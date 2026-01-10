b388e1884344890b4ba18c7226de6a83fff22309
Lalatendu Bal <lalatendu96@gmail.com>
2026-01-10 12:06:10 +0000

    Code challenge submission for question: 6 Zigzag Conversion

--- 6 Zigzag Conversion/6zigzag-conversion.py
+++ 6 Zigzag Conversion/6zigzag-conversion.py
@@ -2,15 +2,13 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
+
         c = 1
         sign = -1
         news = {}
 
-        for _ in range(numRows):
-            news[c] = ""
-            c += 1
-
-        c = 1
+        for i in range(numRows):
+            news[i+1] = ""
 
         for i in s:
             news[c] += i
@@ -20,8 +18,4 @@ class Solution:
 
             c += sign
 
-        res = ""
-        for i in news:
-            res += news[i]
-
-        return res
+        return ''.join(news.values())
