47a2ba998eae974572c9d9e52148c92b3259407c
Lalatendu Bal <lalatendu96@gmail.com>
2026-01-14 17:34:28 +0000

    Code challenge submission for question: 2086 Minimum Number of Food Buckets to Feed the Hamsters

--- 2086 Minimum Number of Food Buckets to Feed the Hamsters/2086minimum-number-of-food-buckets-to-feed-the-hamsters.py
+++ 2086 Minimum Number of Food Buckets to Feed the Hamsters/2086minimum-number-of-food-buckets-to-feed-the-hamsters.py
@@ -1,6 +1,6 @@
 class Solution:
     def minimumBuckets(self, hamsters: str) -> int:
-        if hamsters == "H":
+        if hamsters == "H" or "HHH" in hamsters or hamsters[:2] == "HH" or hamsters[-2:] == "HH":
             return -1 
 
         res = 0
@@ -10,23 +10,10 @@ class Solution:
         if n < 2:
             return n
 
-        if hamsters[:2] == "HH" or hamsters[-2:] == "HH" or "HHH" in hamsters:
-            return -1
-
         idx = [idx[i + 1] - idx[i] for i in range(len(idx) - 1)] + [idx[-1]]
         
         i = 0
         while i < n:
-            if idx[i] > 2:
-                res += 1
-                i += 1
-            
-            elif idx[i] == 2:
-                res += 1
-                i += 2
-
-            else:
-                res += 1
-                i += 1
-         
+            i += 2 if idx[i] == 2 else 1            
+            res += 1
         return res
