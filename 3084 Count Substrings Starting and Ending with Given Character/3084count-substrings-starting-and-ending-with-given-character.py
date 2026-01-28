6ac77bbc47f6f6d64e4e7e3f397804bbfb3d4f1f
Lalatendu Bal <lalatendu96@gmail.com>
2026-01-28 16:41:32 +0000

    Code challenge submission for question: 3084 Count Substrings Starting and Ending with Given Character

--- 3084 Count Substrings Starting and Ending with Given Character/3084count-substrings-starting-and-ending-with-given-character.py
+++ 3084 Count Substrings Starting and Ending with Given Character/3084count-substrings-starting-and-ending-with-given-character.py
@@ -2,4 +2,4 @@ class Solution:
     def countSubstrings(self, s: str, c: str) -> int:
         from math import comb
         count = sum([1 for i in s if i == c])
-        return comb(count, 1) + comb(count, 2)
+        return comb(count, 2) + count
