a76ef2750aa3e0a88d78c4d974a4488a88a6ee55
Lalatendu Bal <lalatendu96@gmail.com>
2026-02-04 22:29:15 +0000

    Code challenge submission for question: 941 Valid Mountain Array

---
+++ 941 Valid Mountain Array/941valid-mountain-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        f1 = 0
+        f2 = 0
+        for i in range(len(arr) - 1):
+            s = arr[i+1] - arr[i]
+            if f2 == 0: 
+                if s > 0:
+                    f1 = 1
+                    continue
+                elif s < 0:
+                    f2 = 1
+                    continue
+                return False
+            else:
+                if s < 0:
+                    continue
+                else:
+                    return False
+        return True if f1 == 1 and f2 == 1 else False
