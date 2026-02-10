630151c73601876415e18c08e73c5bbd9a5d4fee
Lalatendu Bal <lalatendu96@gmail.com>
2026-02-10 13:51:22 +0000

    Code challenge submission for question: 593 Valid Square

---
+++ 593 Valid Square/593valid-square.py
@@ -0,0 +1,8 @@
+class Solution:
+    def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
+        from numpy import square, subtract
+        def dist(p1, p2):
+            return sum(square(subtract(p2, p1)))
+        d = sorted([dist(p1, p2), dist(p2, p3), dist(p3, p4), dist(p4, p1), dist(p1, p3), dist(p4, p2)])
+        print(d)
+        return True if d.count(d[0]) == 4 and d.count(d[4]) == 2 and d[4] == 2 * d[0] else False
