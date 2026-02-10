65b8c84d15f08062cd01f92a5231c05a19ab7887
Lalatendu Bal <lalatendu96@gmail.com>
2026-02-10 13:52:50 +0000

    Code challenge submission for question: 593 Valid Square

--- 593 Valid Square/593valid-square.py
+++ 593 Valid Square/593valid-square.py
@@ -4,5 +4,4 @@ class Solution:
         def dist(p1, p2):
             return sum(square(subtract(p2, p1)))
         d = sorted([dist(p1, p2), dist(p2, p3), dist(p3, p4), dist(p4, p1), dist(p1, p3), dist(p4, p2)])
-        print(d)
         return True if d.count(d[0]) == 4 and d.count(d[4]) == 2 and d[4] == 2 * d[0] else False
