5686778d7f1d07d486720c4fe853cce1fc4149a0
Lalatendu Bal <lalatendu96@gmail.com>
2026-03-07 09:14:19 +0000

    Code challenge submission for question: 4 Median of Two Sorted Arrays

--- 4 Median of Two Sorted Arrays/4median-of-two-sorted-arrays.cpp
+++ 4 Median of Two Sorted Arrays/4median-of-two-sorted-arrays.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-        sort(nums1.begin(), nums1.end());
-        int n = nums1.size();
+        int n1 = nums1.size(), n2 = nums2.size(), i = 0, j = 0;
+        int k = n1 + n2, curr = 0, prev = 0;
 
-        if (n % 2 == 0) {
-            return (double)(nums1[n/2] + nums1[n/2 -1]) / 2;
+        for (int z = 0; z <= k / 2; z++){
+            prev = curr;
+            if (i < n1 && (j >= n2 || nums2[j] > nums1[i])){
+                curr = nums1[i];
+                i++;
+            }
+            else {
+                curr = nums2[j];
+                j++;
+            }
+        }
+
+        if (k % 2 == 0) {
+            return (curr + prev) / 2.0;
         }
         else {
-            return nums1[n/2];
+            return curr;
         }
     }
 };
